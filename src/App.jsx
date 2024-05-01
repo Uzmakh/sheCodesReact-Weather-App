88805691181edf64ebd019fbda98e7506ff0f30b
Uzma Khan <urbazoha@gmail.com>
2024-05-01 08:59:24 +0500

    Added Github & Netlify Link to the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <div className='App'>
         <h1>Weather App</h1>
         <Weather/>
+        <p>open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href="https://ajax-searchengine.netlify.app/">Netlify</a></p>
       </div>
        
     </>
